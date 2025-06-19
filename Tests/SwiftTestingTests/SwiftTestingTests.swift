3dc2d9f954b61fae5c981b10d55027df9caadfa2
Spike Burton <44298491+spikeburton@users.noreply.github.com>
2025-06-18 22:21:49 -0400

    add test suite

--- Tests/SwiftTestingTests/SwiftTestingTests.swift
+++ Tests/SwiftTestingTests/SwiftTestingTests.swift
@@ -1,6 +1,61 @@
 import Testing
 @testable import SwiftTesting
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+struct SwiftTestingTrait: SuiteTrait, TestScoping {
+    func provideScope(for test: Test, testCase: Test.Case?, performing function: () async throws -> Void) async throws {
+        guard let name = test.displayName else {
+            return
+        }
+
+        print("[DEBUG] starting suite: \(name))")
+        try await function()
+        print("[DEBUG] finished suite: \(name))")
+    }
+}
+
+extension Trait where Self == SwiftTestingTrait {
+    static var swiftTestingTrait: Self { Self() }
+}
+
+@Suite("Swift Testing Tests", .serialized, .swiftTestingTrait)
+final class SwiftTestingTests {
+    @Suite("Child One") class ChildOne {
+        init() {
+            print("[DEBUG] child one init")
+        }
+
+        deinit {
+            print("[DEBUG] child one deinit")
+        }
+    }
+
+    @Suite("Child Two") class ChildTwo {
+        init() {
+            print("[DEBUG] child two init")
+        }
+
+        deinit {
+            print("[DEBUG] child two deinit")
+        }
+    }
+}
+
+extension SwiftTestingTests.ChildOne {
+    @Test func testOne() async throws {
+        print("[DEBUG] child one test one")
+    }
+
+    @Test func testTwo() async throws {
+        print("[DEBUG] child one test two")
+    }
+}
+
+extension SwiftTestingTests.ChildTwo {
+    @Test func testOne() async throws {
+        print("[DEBUG] child two test one")
+    }
+
+    @Test func testTwo() async throws {
+        print("[DEBUG] child two test two")
+    }
 }
